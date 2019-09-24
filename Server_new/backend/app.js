47a2782bf49b384f8c025e277f6db4d8b3adda72
YOUNG-WOO NAM <nywoo94@gmail.com>
2019-09-24 18:10:29 +0900

    API PATH RE;

--- Server_new/backend/app.js
+++ Server_new/backend/app.js
@@ -27,7 +27,7 @@ app.use(methodOverride('_method'));
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
 
-app.use('/', indexRouter);
+app.use('/api', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
