e9503069f6a1b621b0435de75215763cf66ddb0b
boncheol <boncheol@boncheolui-MacBookPro.local>
2019-09-25 11:43:56 +0900

    Sentence Select Function
    - 문장 선택시 번역박스에 표시되는 기능구현

--- Server_new/frontend/src/components/views/ProjectView/TranslateView.vue
+++ Server_new/frontend/src/components/views/ProjectView/TranslateView.vue
@@ -12,21 +12,7 @@
       </div>
       <div class="box-body scrollable vh-75" v-bind:style="{ fontSize: fontsize + 'px' }">
           <br> 
-          {{sentences[0].raw_text}}
-          <OriginalText text="abcde" index="1"></OriginalText>
-          
-          <a class="now">
-                                    The background property in CSS can accept comma separated values. "Multiple" backgrounds, if you will. You can also think of them as layered backgrounds since they have a stacking order. </a><br><br><a>
-                                    Unfortunately, that's not valid. I'm not entirely sure why. A while back when I whined on Twitter about it I got a variety of ideas/reasons/excuses. None of them rang quite true for me. It's true you cannot comma-separate background-color, but I don't think that's relevant here as I'm comma separating the background shorthand not specifically background-color (not to mention ordering those values the other way around works fine).</a> <br><br> <a>
-                                    I suspect the real reason that was decided is because it would be too easy for authors to screw up. background: green, url(image.jpg); would "fail" in a sense in that it would just flood the background green. Forcing the color to be last makes the color kind of like a "fallback" rather than a layer like any other.
-                                    </a><br><br>
-          <a>
-                                    Unfortunately, that's not valid. I'm not entirely sure why. A while back when I whined on Twitter about it I got a variety of ideas/reasons/excuses. None of them rang quite true for me. It's true you cannot comma-separate background-color, but I don't think that's relevant here as I'm comma separating the background shorthand not specifically background-color (not to mention ordering those values the other way around works fine).</a> <br><br> <a>
-                                    I suspect the real reason that was decided is because it would be too easy for authors to screw up. background: green, url(image.jpg); would "fail" in a sense in that it would just flood the background green. Forcing the color to be last makes the color kind of like a "fallback" rather than a layer like any other.
-                                    </a><br><br><a>
-                                    Unfortunately, that's not valid. I'm not entirely sure why. A while back when I whined on Twitter about it I got a variety of ideas/reasons/excuses. None of them rang quite true for me. It's true you cannot comma-separate background-color, but I don't think that's relevant here as I'm comma separating the background shorthand not specifically background-color (not to mention ordering those values the other way around works fine).</a> <br><br> <a>
-                                    I suspect the real reason that was decided is because it would be too easy for authors to screw up. background: green, url(image.jpg); would "fail" in a sense in that it would just flood the background green. Forcing the color to be last makes the color kind of like a "fallback" rather than a layer like any other.
-                                    </a><br><br>
+          <OriginalText v-for="(sentence, i) in sentences" :text="sentence.raw_text" :index="i"></OriginalText>
       </div>
   </div>
 </template>
@@ -70,14 +56,6 @@ export default {
 
 <style scoped>
     .box-body > a {
-        font-size:12px;
     }
- .box-body > .now {
-    padding: 5px;
-    line-height: 2.5;
-    border-radius: 30px;
-    color: white;
-    background-image: linear-gradient(to right, #434343 0%, black 100%);
-}
     
 </style>
--- Server_new/frontend/src/store/mutations.js
+++ Server_new/frontend/src/store/mutations.js
@@ -1,8 +1,15 @@
 export default {
   /* -----------------------------------------------------------
     ProjView
+    *Mutation 주의사항 : 한번에 하나의 파라미터밖에 못받음 (단순 setter)
   ----------------------------------------------------------- */
-  /* -----------------------------------------------------------
+  SET_CRURRENT_SENTENCE_TEXT (state, text) {
+    state.crntStcText = text
+  },
+  SET_CRURRENT_SENTENCE_INDEX (state, index) {
+    state.crntStcIndex = index
+  },
+    /* -----------------------------------------------------------
     Co-Pilot Variables (삭제할것)
   ----------------------------------------------------------- */
   TOGGLE_LOADING (state) {
