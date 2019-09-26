b028c3b111c5207a281d8333f5d2775c72a11250
boncheol <boncheol@boncheolui-MacBookPro.local>
2019-09-26 12:54:40 +0900

    translate Text-Eval Print
    - 번역 평가 박스 구현중

--- Server_new/frontend/src/components/views/ProjectView/TranslateEval.vue
+++ Server_new/frontend/src/components/views/ProjectView/TranslateEval.vue
@@ -1,29 +1,17 @@
 <template>
     <div>
-         <div class="trans-list now"><span class="small-alert">내가 지지하는 번역</span>
-          <img src="/static/img/user2-160x160.jpg" alt="User Image" class="user-image circle"><b>익명</b><br> 
-          <div class="contents">
-            CSS의 배경 속성은 쉼표로 구분 된 값을 허용 할 수 있습니다. "여러"배경. 스택 순서가 있기 때문에 배경을 레이어 배경으로 생각할 수도 있습니다.
-          </div></div>
-        <div class="trans-list"><span class="small-alert">번역 1</span>
-          <img src="/static/img/user2-160x160.jpg" alt="User Image" class="user-image circle"><b>익명</b><br> 
-          <div class="contents">
-            CSS의 배경 속성은 쉼표로 구분 된 값을 허용 할 수 있습니다. "여러"배경. 스택 순서가 있기 때문에 배경을 레이어 배경으로 생각할 수도 있습니다.
-          </div></div>
-        <!--각각의 객체를 컴포넌트로 만들기-->
-        <div class="trans-list"><span class="small-alert">번역 2</span>
-          <img src="/static/img/user2-160x160.jpg" alt="User Image" class="user-image circle"><b>익명</b><br> 
-          <div class="contents">
-            CSS의 배경 속성은 쉼표로 구분 된 값을 허용 할 수 있습니다. "여러"배경. 스택 순서가 있기 때문에 배경을 레이어 배경으로 생각할 수도 있습니다.
-          </div></div>
+        <TranslationText></TranslationText>
     </div>
 </template>
 
 <script>
 // import axios from 'axios'
-
+import TranslationText from './TranslationText'
 export default {
   name: 'TranslateEval',
+  component: {
+    TranslationText
+  },
   data () {
     return {}
   },
---
+++ Server_new/frontend/src/components/views/ProjectView/TranslationText.vue
@@ -0,0 +1,49 @@
+<template>
+        
+    <div class="trans-list"><span class="small-alert">번역 번호</span>
+            <img src="/static/img/user2-160x160.jpg" alt="User Image" class="user-image circle"><b>익명</b><br> 
+            <div class="contents">
+            텍스트
+            </div>
+        </div>
+        
+</template>
+
+<script>
+// import axios from 'axios'
+
+export default {
+  name: 'TranslationText',
+  props: ['text', 'index'],
+  data () {
+    return {}
+  },
+  methods: {
+    // 현재 선택한 문장 전역변수화 Vuex
+    changeCurrent (num) {
+      this.$store.commit('SET_CRURRENT_SENTENCE_INDEX', this.index)
+      this.$store.commit('SET_CRURRENT_SENTENCE_TEXT', this.text)
+    }
+  },
+  mounted () {
+  }
+}
+</script>
+
+<style scoped>
+    
+ .contents {
+        padding-top:10px;
+    }
+    
+    .now {
+        background-color:#5CD590;
+        color:white;
+    }
+    
+    .now > .small-alert {
+        background-color:white;
+        color:#5CD590;
+    }
+    
+</style>
