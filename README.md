c1d7b07650c7fd887e71e8180fe2b63961323b23
boncheol <boncheol@boncheolui-MacBookPro.local>
2019-10-01 12:33:05 +0900

    Project Add Page Designed
    - 프로젝트 생성 페이지 프론트엔드 완성
    - 이미지 업로드 기능은 일단 보류

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Blot
+
+
+##개발 노트
+###VUEX
+- State(전역변수)의 SETTER
+    this.$store.commit('SET_CRURRENT_SENTENCE_INDEX', this.index)
+- Vue Template 에서의 State 읽기
+    this.$store.state.crntIcon
+- Vue Methods 에서의 State 읽기
+    $stroe.state.crntIcon
+
+###VUE
+- 스타일 바인딩
+    v-bind:class="클래스이름"
+    v-bind:class="{클래스이름:조건식}"
+    v-bind:class="[클래스이름,{클래스이름:조건식}]"
--- Server_new/frontend/src/components/views/ProjectAdd/IconSelector.vue
+++ Server_new/frontend/src/components/views/ProjectAdd/IconSelector.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div v-for="(icon,index) in iconArray" class="icon-box">
-        <i v-bind:class="icon"></i>
+        <i v-bind:class="[icon, {now: $store.state.crntIcon === icon}]" v-on:click="setIcon(icon)"></i>
     </div>
   </div>
 </template>
@@ -14,12 +14,17 @@ export default {
   data () {
     return {
       iconArray: [
-        'ri-home-3-line', 'ri-building-2-line', 'ri-cloud-line', 'ri-bar-chart-grouped-line', 'ri-global-line',
+        'ri-questionnaire-line', 'ri-building-2-line', 'ri-cloud-line', 'ri-bar-chart-grouped-line', 'ri-global-line',
         'ri-stack-line', 'ri-chat-3-line', 'ri-brush-4-line', 'ri-palette-line', 'ri-git-repository-private-line'
       ]
     }
   },
-  methods: {},
+  methods: {
+    setIcon (iconName) {
+      this.$store.commit('SET_CURRENT_ICON', iconName)
+      console.log(iconName)
+    }
+  },
   mounted () {}
 }
 </script>
@@ -35,6 +40,8 @@ export default {
   }
   .icon-box i {
     padding:10px;
-    border:1px solid red;
+  }
+  .now {
+    border: 1px solid #5CD590;
   }
 </style>
