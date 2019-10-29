44d804e0c37f8d1aaa4d4372a53bc92c5a5ed5f6
YOUNG-WOO NAM <nywoo94@gmail.com>
2019-10-29 19:13:52 +0900

    sync commit

--- Server_new/backend/blockchain/contractInfo.js
+++ Server_new/backend/blockchain/contractInfo.js
@@ -1,15 +1,15 @@
 const fs = require('fs');
 
 module.exports = {
-    DEPLOYED_BLOTTOKEN_ABI : fs.existsSync('./metadata/BlotToken_ABI') && fs.readFileSync('./metadata/BlotToken_ABI', 'utf8'),
-    DEPLOYED_BLOTTOKEN_ADDRESS : fs.readFileSync('./metadata/BlotToken_Address', 'utf8').replace(/\n|\r/g, ""),
+    DEPLOYED_BLOTTOKEN_ABI : fs.existsSync('./blockchain/metadata/BlotToken_ABI') && fs.readFileSync('./blockchain/metadata/BlotToken_ABI', 'utf8'),
+    DEPLOYED_BLOTTOKEN_ADDRESS : fs.readFileSync('./blockchain/metadata/BlotToken_Address', 'utf8').replace(/\n|\r/g, ""),
 
-    DEPLOYED_BLOTPROJECT_ABI : fs.existsSync('./metadata/BlotProject_ABI') && fs.readFileSync('./metadata/BlotProject_ABI', 'utf8'),
-    DEPLOYED_BLOTPROJECT_ADDRESS : fs.readFileSync('./metadata/BlotProject_Address', 'utf8').replace(/\n|\r/g, ""),
+    DEPLOYED_BLOTPROJECT_ABI : fs.existsSync('./blockchain/metadata/BlotProject_ABI') && fs.readFileSync('./blockchain/metadata/BlotProject_ABI', 'utf8'),
+    DEPLOYED_BLOTPROJECT_ADDRESS : fs.readFileSync('./blockchain/metadata/BlotProject_Address', 'utf8').replace(/\n|\r/g, ""),
 
-    DEPLOYED_BLOTUSER_ABI : fs.existsSync('./metadata/BlotUser_ABI') && fs.readFileSync('./metadata/BlotUser_ABI', 'utf8'),
-    DEPLOYED_BLOTUSER_ADDRESS : fs.readFileSync('./metadata/BlotUser_Address', 'utf8').replace(/\n|\r/g, ""),
+    DEPLOYED_BLOTUSER_ABI : fs.existsSync('./blockchain/metadata/BlotUser_ABI') && fs.readFileSync('./blockchain/metadata/BlotUser_ABI', 'utf8'),
+    DEPLOYED_BLOTUSER_ADDRESS : fs.readFileSync('./blockchain/metadata/BlotUser_Address', 'utf8').replace(/\n|\r/g, ""),
 
-    DEPLOYED_BLOTMAIN_ABI : fs.existsSync('./metadata/BlotMain_ABI') && fs.readFileSync('./metadata/BlotMain_ABI', 'utf8'),
-    DEPLOYED_BLOTMAIN_ADDRESS : fs.readFileSync('./metadata/BlotMain_Address', 'utf8').replace(/\n|\r/g, "")
+    DEPLOYED_BLOTMAIN_ABI : fs.existsSync('./blockchain/metadata/BlotMain_ABI') && fs.readFileSync('./blockchain/metadata/BlotMain_ABI', 'utf8'),
+    DEPLOYED_BLOTMAIN_ADDRESS : fs.readFileSync('./blockchain/metadata/BlotMain_Address', 'utf8').replace(/\n|\r/g, "")
 };
