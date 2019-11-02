f71c69fd03b1defbc42377f9c08d9728c46cb8c7
nablekim94 <nablekim94@gmail.com>
2019-11-02 17:44:59 +0900

    modified sample userId name to front side.

--- Server_new/frontend/src/components/layout/SeungSooTest.vue
+++ Server_new/frontend/src/components/layout/SeungSooTest.vue
@@ -20,12 +20,12 @@ export default {
   methods: {
     // 프로젝트 정보를 블록체인 상에 등록하기 위해 서버에 대납 요청을 보내는 로직
     addProject() {
-      var payload = {'projectId': 'projectId7', 'writerId': 'kim', 'deadline': '2019.11.03', 'reward': 30}
+      var payload = {'projectId': 'projectId7', 'writerId': 'u1', 'deadline': '2019.11.03', 'reward': 30}
       this.$store.dispatch('CREATE_NEW_PROJECT', payload)
     },
     // 블록체인으로부터 토큰을 구매하기 위해 서버에 대납 요청을 보내는 로직
     purchaseToken() {
-      var payload = {'userAddress': 'kim', 'klayNum': 1}
+      var payload = {'userAddress': 'u1', 'klayNum': 1}
       this.$store.dispatch('PURCHASE_BLOT_TOKEN', payload)
     },
     // 구매한 토큰을 다시 Klay 코인으로 바꿔가기 위해 서버에 대납 요청을 보내는 로직
@@ -41,7 +41,7 @@ export default {
     // 사용자 지갑 주소를 변경하는 함수
     changeAddress() {
       // @ NOTE 사용자 지갑 주소를 변경하려면, 기존에 사용자가 등록한 지갑 계정으로 블록체인 로그인을 한 상태에서 지갑 변경을 시도해야함
-      var payload = {'userId': 'kim', 'newUserAddress': '새로운_지갑_주소_입력'}
+      var payload = {'userId': 'u1', 'newUserAddress': '새로운_지갑_주소_입력'}
       this.$store.dispatch('CHANGE_USER_WALLET_ADDR', payload)
     },
   },
