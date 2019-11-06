7f008f949f9f5c6a6dd77fa592c5f9576809640b
nablekim94 <nablekim94@gmail.com>
2019-11-07 05:11:26 +0900

    Added parameters to update UserRelibility with projectId and changed reason.

--- Server_new/backend/blockchain/contract.js
+++ Server_new/backend/blockchain/contract.js
@@ -4,10 +4,9 @@ const config = { rpcURL: 'https://api.baobab.klaytn.net:8651/'}
 
 const cav = new Caver(config.rpcURL);
 
-const blotTokenContract = new cav.klay.Contract(JSON.parse(contractInfo.DEPLOYED_BLOTTOKEN_ABI), contractInfo.DEPLOYED_BLOTTOKEN_ADDRESS);
-const blotProjectContract = new cav.klay.Contract(JSON.parse(contractInfo.DEPLOYED_BLOTPROJECT_ABI), contractInfo.DEPLOYED_BLOTPROJECT_ADDRESS);
-const blotUserContract = new cav.klay.Contract(JSON.parse(contractInfo.DEPLOYED_BLOTUSER_ABI), contractInfo.DEPLOYED_BLOTUSER_ADDRESS);
-const blotMainContract = new cav.klay.Contract(JSON.parse(contractInfo.DEPLOYED_BLOTMAIN_ABI), contractInfo.DEPLOYED_BLOTMAIN_ADDRESS);
+const blotTokenContract = new cav.klay.Contract(contractInfo.DEPLOYED_BLOTTOKEN_ABI, contractInfo.DEPLOYED_BLOTTOKEN_ADDRESS);
+const blotProjectContract = new cav.klay.Contract(contractInfo.DEPLOYED_BLOTPROJECT_ABI, contractInfo.DEPLOYED_BLOTPROJECT_ADDRESS);
+const blotUserContract = new cav.klay.Contract(contractInfo.DEPLOYED_BLOTUSER_ABI, contractInfo.DEPLOYED_BLOTUSER_ADDRESS);
 
 const privatekey = '0xa86989e8db32b234489fd06e6f622e8913777372f7553363c0bed137e32315f5';
 
@@ -19,7 +18,7 @@ module.exports = function() {
     
     return {
         /* 사용자 관련 */
-        // 회원 가입 시, 블록체인 상에 회원 정보 저장 O
+        // 회원 가입 시, 블록체인 상에 회원 정보 저장
         userSignUp : async function(userId, walletAddress) {
           // @ NOTE : value에 양수 or 음수를 주면 신뢰도 증가/감소 가능
           return await blotUserContract.methods.userSignUp(userId, walletAddress).send({
@@ -28,29 +27,30 @@ module.exports = function() {
           });
         },
         
-        // userId로 사용자 신뢰 점수 조회 O
+        // userId로 사용자 신뢰 점수 조회
         getTrust : async function(userId) {
             return await blotUserContract.methods.getUserReliabilityByUserId(userId).call();
         },
         
-        // userId로 사용자 지갑 주소 조회 O
+        // userId로 사용자 지갑 주소 조회
         getWalletAddress : async function(userId) {
             return await blotUserContract.methods.getUserAddressByUserId(userId).call();
         },
         
-        // 사용자 신뢰도 조정 함수 O
-        setTrust : async function(userId, value) {
+        // 사용자 신뢰도 조정 함수
+        setTrust : async function(projectId, userId, value, reason) {
             // @ NOTE : value에 양수 or 음수를 주면 신뢰도 증가/감소 가능
-            return await blotMainContract.methods.updateUserReliability(userId, value).send({
+            // @ Params : projectId, userId, 증감수치, 변경이유(0: 번역참가, 1:평가참가, 2:월별 점수 차감)
+            return await blotUserContract.methods.updateUserReliability(projectId, userId, value, reason).send({
                 from : this._getFeePayerWalletAddress(),
                 gas : 250000
             });
         },
 
         /* 프로젝트 관련 */
-        // 프로젝트 정보 저장 O
+        // 프로젝트 정보 저장
         createProject : async function(projectId, writerId, deadline, reward) {
-          return await blotMainContract.methods.registerNewProject(
+          return await blotProjectContract.methods.registerNewProject(
             projectId, 
             writerId, 
             deadline, 
@@ -61,23 +61,24 @@ module.exports = function() {
           });
         },
 
-        // ProjectId로 프로젝트에 걸린 보상금 조회 O
+        // ProjectId로 프로젝트에 걸린 보상금 조회
         getReward : async function(projectId) {
           return await blotProjectContract.methods.getProjectRewardByprojectId(projectId).call();
         },
         
+        // userId로 잔고 조회
         getBalance : async function(userId) {
-          return await blotMainContract.methods.getUserBalanceByUserId(userId).call();
+          return await blotUserContract.methods.getUserBalanceByUserId(userId).call();
         },
 
-        // ProjectId로 프로젝트에 정보(글쓴이, 마감일, 보상금) 조회 O
+        // ProjectId로 프로젝트에 정보(글쓴이, 마감일, 보상금) 조회
         getProjectInfo : async function(projectId) {
           return await blotProjectContract.methods.getProjectInfoByprojectId(projectId).call();
         },
         
-        // 번역 활동 기록 O
+        // 번역 활동 기록
         setTranslation : async function(projectId, translatorId, sentenceList, translationList, share) {
-            return await blotMainContract.methods.generateTranslationEvent(
+            return await blotProjectContract.methods.generateTranslationEvent(
               projectId, 
               translatorId, 
               sentenceList, 
@@ -89,9 +90,9 @@ module.exports = function() {
             });
         },
         
-        // 평가 활동 기록 O
+        // 평가 활동 기록
         setEvaluation : async function(projectId, evaluatorId, sentenceList, translationList, share) {
-            return await blotMainContract.methods.generateEvaluationEvent(
+            return await blotProjectContract.methods.generateEvaluationEvent(
                 projectId, 
                 evaluatorId, 
                 sentenceList, 
@@ -103,9 +104,10 @@ module.exports = function() {
             });
         },
         
-        // 번역 마감 후 보상금 지급 O
+        // 번역 마감 후 보상금 지급
+        // @params : recipient 수신자 지갑주소, reward 보상금
         Transfer : async function(recipient, reward) {
-            return await blotMainContract.methods.sendRewardToUser(recipient, reward).send({
+            return await blotTokenContract.methods.sendRewardToUser(recipient, reward).send({
                 from : this._getFeePayerWalletAddress(),
                 gas : 250000
             });
@@ -114,7 +116,7 @@ module.exports = function() {
         // 컨트랙트 계정으로부터 대납자 계정으로 잉여 자금 보내기 O
         // @ NOTE : 번역 마감 후 보상금 분배, 번역/평가 활동 기록 로직 종료 후 하루 한번만 실행할 것
         chargeFeePayerBalance : async function() {
-            return await blotMainContract.methods.transferBalanceToOwner().send({
+            return await blotTokenContract.methods.transferBalanceToOwner().send({
                 from : this._getFeePayerWalletAddress(),
                 gas : 250000
             });
@@ -130,16 +132,16 @@ module.exports = function() {
 
         // 번역 활동 기록 불러오기
         getTranslationLog : async function() {
-            return await blotMainContract.getPastEvents('NewTranslation', {
-              filter: '',
-              fromBlock: 0,
-              toBlock: 'latest'          
-            });
-          },
+          return await blotProjectContract.getPastEvents('NewTranslation', {
+            filter: '',
+            fromBlock: 0,
+            toBlock: 'latest'          
+          });
+        },
     
         //  평가 활동 기록 불러오기
         getEvaluationLog : async function() {
-          return await blotMainContract.getPastEvents('NewEvaluation', {
+          return await blotProjectContract.getPastEvents('NewEvaluation', {
             filter: '',
             fromBlock: 0,
             toBlock: 'latest'          
@@ -158,10 +160,9 @@ module.exports = function() {
         },
   
         // 컨트랙트의 Klay 수신 내역 불러오기(토큰 구매 기록을 확인할 수 있음)
-        // @ NOTE : 어느 계정 주소에서 얼마만큼의 Klay를 송금했는가 기록이 남아있음
+        // @ NOTE : 어느 계정 주소에서 얼마만큼의 Klay를 송금했는가(Peb단위로 표현) 기록이 남아있음
         getTokenPurchaseLog : async function() {
-  
-          return await blotMainContract.getPastEvents('LogDepositReceived', {
+          return await blotTokenContract.getPastEvents('LogDepositReceived', {
             filter: '',
             fromBlock: 0,
             toBlock: 'latest'          
@@ -178,6 +179,12 @@ module.exports = function() {
   
         // 사용자별 신뢰도 조정 기록
         // @ NOTE : 어떤 사용자의 신뢰점수가 얼마나 증가/감소하여 몇점이 되었는지 기록되어 있음(신뢰 점수 변화의 발자취 기록)
+        // @ returns : 
+          // projectId
+          // userId
+          // 조정한 수치 value
+          // 변경 이유(0:번역참여, 1:평가참여, 2:월별 차감) reason, 
+          // 조정 후 신뢰도 점수 totalUserReliability
         getReliabilityLog : async function() {
           return await blotUserContract.getPastEvents('Reliability', {
             filter: '',
@@ -188,12 +195,13 @@ module.exports = function() {
           /*
           // JSON 객체 형태로 리턴되기 때문에 결과값을 사용하려면 아래와 같이 접근해서 사용해야함
           for(var i=0; i<result.length; i++) {
+            console.log('projectId: : '+JSON.stringify(result[i].returnValues.projectId));
             console.log('userId : '+JSON.stringify(result[i].returnValues.userId));
             console.log('value : '+JSON.stringify(result[i].returnValues.value));
+            console.log('reason: : '+JSON.stringify(result[i].returnValues.reason));
             console.log('totalUserReliability : '+JSON.stringify(result[i].returnValues.totalUserReliability));
           }
           */
-          
         },
   
   
