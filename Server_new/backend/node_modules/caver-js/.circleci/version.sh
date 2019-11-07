6dbd53f7c4a9cfbf27b28a5265dc08014109c42e
boncheol <boncheol@boncheolui-MacBookPro.local>
2019-11-07 14:21:34 +0900

    node_module conflict fix
    fix

--- Server_new/backend/node_modules/caver-js/.circleci/version.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#take version from package.json
-if [ -f package.json ]; then
-    version_package=$(awk '/version/ {gsub("\"",""); print $2}' package.json | tr -d ',')
-    echo $version_package
-else
-    echo "package.json not exist"
-fi
