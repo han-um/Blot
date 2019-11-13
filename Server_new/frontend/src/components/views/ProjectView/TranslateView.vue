300c779d0c7581267a885d13dd01fe5b70881716
han-um <humn4393@gmail.com>
2019-11-13 22:41:23 +0900

    Ended Project Translate vew Dev
    - 마감처리시 번역보기 페이지 개발중
    - 개발중 머지를 위한 커밋

--- Server_new/frontend/src/components/views/ProjectView/TranslateView.vue
+++ Server_new/frontend/src/components/views/ProjectView/TranslateView.vue
@@ -3,8 +3,8 @@
       <div class="box-header with-border"><i class="ri-t-box-line"></i> 내용</div>
       <div class="box-header-menu">
          <ul>
-           <li v-on:click="crntMenu = 'original'" v-bind:class="{active : crntMenu === 'original'}">원본보기</li>
-           <li v-if="this.$store.state.crntProjEnded" v-on:click="crntMenu = 'translate'" v-bind:class="{active : crntMenu === 'translate'}">번역보기</li>
+           <li v-on:click="setMenuToOriginal()" v-bind:class="{active : crntMenu === 'original'}">원본보기</li>
+           <li v-if="this.$store.state.crntProjEnded" v-on:click="setMenuToTranslate()" v-bind:class="{active : crntMenu === 'translate'}">번역보기</li>
            <li class="icon" style="float:right" v-on:click="fontsize--" v-if="fontsize>10"><i class="ri-subtract-fill"></i></li>
            <li class="icon disabled" style="float:right" v-if="fontsize<=10"><i class="ri-subtract-fill"></i></li>
            <li class="icon" style="float:right" v-on:click="fontsize++"><i class="ri-add-line"></i></li>
@@ -16,6 +16,7 @@
           <OriginalText v-if="crntMenu === 'original'" v-for="(sentence, i) in sentences" :text="sentence.raw_text" :index="i"></OriginalText>
           <OriginalText v-if="crntMenu === 'translate'" v-for="(sentence, i) in sentences" :text="sentence.raw_text" :index="i"></OriginalText>   
       </div>
+      {{transSentences}}
   </div>
 </template>
 
@@ -45,9 +46,6 @@ export default {
             return
           }
           this.sentences = response.data.sentence
-          // 제일 처음엔 0번 문장으로 전역변수 변경
-          var payload = {'index': 0, 'text': this.sentences[0].raw_text}
-          this.$store.dispatch('REFRESH_CURRENT_SENTENCE', payload)
         })
         .catch(error => {
           // Request failed.
@@ -57,11 +55,54 @@ export default {
     },
     getTransSentences () {
         // 문장처리
+      axios.get('/api/project/' + this.$route.params.id + '/sentence/finalTrans')
+      .then(response => {
+        if (response.status !== 200) {
+          this.error = response.statusText
+          return
+        }
+        this.transSentences = response.data
+        // 제일 처음엔 0번 문장으로 전역변수 변경
+      })
+    },
+    setMenuToOriginal() {
+      axios.get('/api/project/' + this.$route.params.id + '/sentence')
+      .then(response => {
+        if (response.status !== 200) {
+          this.error = response.statusText
+          return
+        }
+        this.sentences = response.data.sentence
+        this.crntMenu = 'original'
+        var payload = {'index': 0, 'text': this.sentences[0].raw_text}
+        this.$store.dispatch('REFRESH_CURRENT_SENTENCE', payload)
+      })
+      .catch(error => {
+        // Request failed.
+        console.log('error', error.response)
+        this.error = error.response.statusText
+      })
+    },
+    setMenuToTranslate() {
+      axios.get('/api/project/' + this.$route.params.id + '/sentence/finalTrans')
+      .then(response => {
+        if (response.status !== 200) {
+          this.error = response.statusText
+          return
+        }
+        this.transSentences = response.data
+        this.crntMenu = 'translate'
+        var payload = {'index': 0, 'text': this.transSentences[0]}
+        this.$store.dispatch('REFRESH_CURRENT_SENTENCE', payload)
+        // 제일 처음엔 0번 문장으로 전역변수 변경
+      })
     }
   },
   mounted () {
     this.getSentences()
+    this.setMenuToOriginal()
     if (this.$store.state.crntProjEnded) {
+      console.log('???')
       this.getTransSentences()
     }
   }
