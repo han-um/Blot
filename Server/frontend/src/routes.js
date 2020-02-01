f13aa03b946acb7cc3711b6ca5a179e87b2ea556
han-um <boncheolb@gmail.com>
2020-02-01 15:31:59 +0900

    Change to Hash mode / Rebuild Frontend
    - nginx호환을 위해 Hash mode로 전환
    - 프론트엔드 리빌드
    - 불필요한 코드 제거 (from CoPilot)

--- Server/frontend/src/routes.js
+++ Server/frontend/src/routes.js
@@ -17,15 +17,6 @@ import ProjectViewOverview from './components/views/ProjectView/Overview.vue'
 import ProjectViewTranslate from './components/views/ProjectView/Translate.vue'
 import ProjectViewUsers from './components/views/ProjectView/Users.vue'
 
-//  Dash
-import DashboardView from './components/views/Dashboard.vue'
-import TablesView from './components/views/Tables.vue'
-import TasksView from './components/views/Tasks.vue'
-import SettingView from './components/views/Setting.vue'
-import AccessView from './components/views/Access.vue'
-import ServerView from './components/views/Server.vue'
-import ReposView from './components/views/Repos.vue'
-
 // BlockChainLogin
 import BlockChainLogin from './components/views/BlockChain/BlockChainLogin.vue'
 
@@ -63,42 +54,6 @@ const routes = [
     component: DashView,
     children: [
       {
-        path: 'dashboard',
-        alias: '',
-        component: DashboardView,
-        name: 'Dashboard',
-        meta: {description: 'Overview of environment'}
-      }, {
-        path: 'tables',
-        component: TablesView,
-        name: 'Tables',
-        meta: {description: 'Simple and advance table in CoPilot'}
-      }, {
-        path: 'tasks',
-        component: TasksView,
-        name: 'Tasks',
-        meta: {description: 'Tasks page in the form of a timeline'}
-      }, {
-        path: 'setting',
-        component: SettingView,
-        name: 'Settings',
-        meta: {description: 'User settings page'}
-      }, {
-        path: 'access',
-        component: AccessView,
-        name: 'Access',
-        meta: {description: 'Example of using maps'}
-      }, {
-        path: 'server',
-        component: ServerView,
-        name: 'Servers',
-        meta: {description: 'List of our servers', requiresAuth: true}
-      }, {
-        path: 'repos',
-        component: ReposView,
-        name: 'Repository',
-        meta: {description: 'List of popular javascript repos'}
-      }, {
         path: 'projview/:id',
         component: ProjectView,
         props: true,
